28e969810f40dd3f691a24ab3a962ea9f106db20
lizhiyong <protonlzy@gmail.com>
2014-08-16 19:35:45 +0800

    c语言学习笔记，文件操作相关工程创建

---
+++ FileUtil/FileUtil.c
@@ -0,0 +1,49 @@
+/*
+ * FileUtil.c
+ *
+ *  Created on: 2014-8-16
+ *      Author: lizhiyong
+ */
+
+#include <stdio.h>
+#include "FileUtil.h"
+
+
+int main(int argc, char *argv[]) {
+	//printf("这么多年了还是只会写hello world\n");
+
+//	printf("参数个数 == %d  \n", argc);
+//
+//    char ** temp = argv;  /* 保留argv */
+//    while( *++temp != NULL ){
+//        printf("%s \n", *temp);
+//    }
+
+	if (argc <= 1) {
+		printf("参数错误 -h 查看使用方法\n");
+		return 0;
+	}
+
+	if (help(argv[1]) == 0) {
+		printf("fileutil -r filename 获取文件内容\n");
+		printf("fileutil -w filename content 将content写入到filename\n");
+		return 0;
+	}
+
+
+	if (argc == 2 && argv[1] != "-h") {
+		printf("参数错误 -h 查看使用方法\n");
+		return 0;
+	}
+
+
+
+	printf("%s \n", argv[0]);
+
+	return 0;
+}
+
+int help(char *input) {
+	char *h = HELP;
+	return strcmp(input, h);
+}
---
+++ FileUtil/FileUtil.h
@@ -0,0 +1,15 @@
+/*
+ * FileUtil.h
+ *
+ *  Created on: 2014-8-16
+ *      Author: lizhiyong
+ */
+
+#ifndef FILEUTIL_H_
+#define FILEUTIL_H_
+
+#define  HELP '-h';
+
+int hello(char *input);
+
+#endif /* FILEUTIL_H_ */
